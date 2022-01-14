125f8e14a983b2e9d375bce52b97feaec0e82504
Gabriela Flores <gabriela.floze@gmail.com>
2022-01-13 23:16:26 -0600

    practica de operadores, captura de datos y variables

---
+++ conversor.py
@@ -0,0 +1,10 @@
+from this import d
+
+
+pesos = input("¿cuantos pesos colombianos tienes? \n")
+pesos = float(pesos)
+# valor_dolar = 3968
+# dolares = pesos / valor_dolar
+# dolares = round(dolares, 2)
+# dolares = str(dolares)
+# print("Tienes $" + dolares + " dolares")
