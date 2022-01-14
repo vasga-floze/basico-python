3d5198fe0600ae2a2fb193b96a070b54f95bf0f3
Gabriela Flores <gabriela.floze@gmail.com>
2022-01-13 23:19:38 -0600

    practica 1 de condicionales

---
+++ condicionales.py
@@ -0,0 +1,7 @@
+edad = int(input("escribe tu edad: \n"))
+if edad > 17:
+    print("Eres mayor de  edad")
+elif edad == 17:
+    print("Estas proximo a ser mayor de edad")
+else:
+    print("Eres menor de edad")
