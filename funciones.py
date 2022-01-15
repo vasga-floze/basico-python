62fa8e4399cf9b05bc26a2c3a8c5383fae9f37e6
Gabriela Flores <gabriela.floze@gmail.com>
2022-01-15 16:50:11 -0600

    funciones

---
+++ funciones.py
@@ -0,0 +1,30 @@
+#*******funciones*******
+
+# def imprimir_mensaje():
+#     print('Mensaje especial: ')
+#     print('Estoy aprendiendo a usar funciones!')
+
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+#*****funciones con parametros*****
+def conversacion(mensaje):
+    print('Hola')
+    print('¿Cómo estás?')
+    print(mensaje)
+    print('Bye')
+
+
+opcion = int(input('Elige una opción (1, 2, 3):'))
+if opcion == 1:
+    #llamado a la funcion, pasando el parametro
+    conversacion('Elegiste la opcion 1')
+elif opcion == 2:
+    #llamado a la funcion, pasando el parametro
+    conversacion('Elegiste la opcion 2')
+elif opcion == 3:
+    #llamado a la funcion, pasando el parametro
+    conversacion('Elegiste la opcion 3')
+else:
+    print('Escribe una opción válida.')
